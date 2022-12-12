e7ee20ca553504498b4fc7ea4575614d51137718
Ritik sharma <98308171+Emritik@users.noreply.github.com>
2022-12-12 14:29:52 +0530

    Update sign.js

--- javascript/sign.js
+++ javascript/sign.js
@@ -1,18 +1,65 @@
-function show() 
+function display()
+{    
+    let pass1,cpass1,txt;
+    pass1=document.getElementById("password").value;
+    cpass1=document.getElementById("confirm-password").value;
+
+    if (pass1 == "")
+    {
+        txt="Please enter the password";
+        document.getElementById("dis").disabled= true;
+        document.getElementById("dis").style.background="none";
+    }
+    else if (cpass1 == "")
+    {
+        txt="Please enter confirm-password";
+        document.getElementById("dis").disabled= true;
+        document.getElementById("dis").style.background="none";
+    }
+    else if (pass1!=cpass1)
+    {
+        txt="Password is not matched";
+        document.getElementById("dis").disabled= true;
+        document.getElementById("dis").style.background="none";
+        
+    }
+    else
+    {
+        txt="Password is same"; 
+        document.getElementById("dis").disabled= false;  
+        document.getElementById("dis").style.background="MediumTurquoise";
+    }
+    document.getElementById("print").innerHTML=txt;
+
+}
+function num()
 {
-    let data=document.getElementById("phone").value;
-    alert("OTP is sent on " +  data + " Number");
-    document.getElementById("para").innerHTML=""    
+    var tel = document.getElementById("phone").value;
+    if(isNaN(tel))
+    {
+        document.getElementById("tels").innerHTML = "Please enter numbers only";
+        document.getElementById("dis").disabled= true;
+        document.getElementById("dis").style.background="none";
+    }
+    else
+    {
+        document.getElementById("tels").innerHTML = "";
+        document.getElementById("dis").disabled= false;
+        document.getElementById("dis").style.background="MediumTurquoise";
+    }
 }
 
-var pass,cpass;
-pass=document.getElementById("password").value;
-cpass=document.getElementById("confirm-password").value;
-function check()
+function datechecker()
 {
-if(pass!=cpass)
-{
-    document.getElementById("print").innerHTML="Password not matched"
-    console.log("print");
+    var date=new Date();
+    var birthday= document.getElementById("date").value;
+    var difference=date.getDate() - birthday.getDate();
+    console(difference);
+
+    
 }
+
+function submit()
+{
+    alert("Your from has been submited successfully,Thank you for visit");
 }
